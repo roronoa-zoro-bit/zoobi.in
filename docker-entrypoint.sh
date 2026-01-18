1d486c6fc89d646dc6c12151685a4a9ef9ade09b
roronoa-zoro-bit <renzosuthar@gmail.com>
2026-01-18 14:14:53 +0530

    Fix: Pass JDBC URL as JVM system property instead of env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Convert DATABASE_URL from postgres:// to jdbc:postgresql://
 if [ -n "$DATABASE_URL" ]; then
-  export SPRING_DATASOURCE_URL=$(echo $DATABASE_URL | sed 's/^postgres:/jdbc:postgresql:/')
+  JDBC_DATABASE_URL=$(echo $DATABASE_URL | sed 's/^postgres:/jdbc:postgresql:/')
+  exec java -Dserver.port=${PORT:-8080} -Dspring.datasource.url="$JDBC_DATABASE_URL" -jar app.jar
+else
+  exec java -Dserver.port=${PORT:-8080} -jar app.jar
 fi
 
-# Start the application
-exec java -Dserver.port=${PORT:-8080} -jar app.jar
